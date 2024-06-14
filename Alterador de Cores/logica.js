affc99ce8fbef81d50737cf52f5c86e6f0299e6f
João Antônio A. D. S. S. <joao.antonio.amorim.8@gmail.com>
2024-06-14 17:31:36 -0300

    Adições e bugs
    - Adicionado cores ao elementos "escrita".
    
    - Encontrado bug neste mesmo elemento quando a cor de fundo altera e o cursos do mouse entra e sai do elemento "escrita" .

--- Alterador de Cores/logica.js
+++ Alterador de Cores/logica.js
@@ -3,41 +3,87 @@
     let fundo = document.querySelector('body#Fundo')
     // let fundo , conectada com <body>Fundo</body>
     let escrita = document.querySelector('h1#cordefundoescrita')
+    escrita.addEventListener('mouseenter' , functentrada)
+    escrita.addEventListener('mouseout' , functsaida)
     //let escrita, conectado com <h1>"Cor de Fundo"</h1>
-
 
 
 function clicado() {
     
     cliques += 1
-
+    
     switch (cliques) {
-        
+            
         case 1:
             fundo.style.backgroundColor = 'green'
             escrita.innerText = 'Verde'
             break
-
+    
         case 2:
             fundo.style.backgroundColor = 'red'
             escrita.innerText = 'Vermelho'
             break
-
+    
         case 3:
             fundo.style.backgroundColor = 'orange'
             escrita.innerText = 'Laranja'
             break
-        
+            
         case 4: 
             fundo.style.backgroundColor = 'gray'
             escrita.innerText = 'Cinza'
             break
-
+    
         case 5:
             fundo.style.backgroundColor =  'blue'
             escrita.innerText = 'Azul'
             break
-
+    
     }
-
+    
 }
+
+
+function functentrada() {
+    escrita.style.color = 'black'
+    escrita.style.background = 'white'
+    if (cliques == 1) {
+        escrita.style.color = 'green'
+        escrita.style.background = 'white'
+    } else if (cliques == 2) {
+        escrita.style.color = 'red'
+        escrita.style.background = 'white'     
+    } else if (cliques == 3) {
+        escrita.style.color = 'orange'
+        escrita.style.background = 'white'     
+    } else if (cliques == 4) {
+        escrita.style.color = 'gray'
+        escrita.style.background = 'white'     
+    } else if (cliques == 5) {
+        escrita.style.color = 'blue'
+        escrita.style.background = 'white'     
+    }
+}
+
+function functsaida() {
+    escrita.style.color = 'white'
+    escrita.style.background = 'black'
+    if (cliques == 1) {
+        escrita.style.color = 'white'
+        escrita.style.background = 'green'
+    } else if (cliques == 2) {
+        escrita.style.color = 'white'
+        escrita.style.background = 'red'     
+    } else if (cliques == 3) {
+        escrita.style.color = 'white'
+        escrita.style.background = 'orange'     
+    } else if (cliques == 4) {
+        escrita.style.color = 'white'
+        escrita.style.background = 'gray'     
+    } else if (cliques == 5) {
+        escrita.style.color = 'white'
+        escrita.style.background = 'blue'     
+    }
+}
+
+
